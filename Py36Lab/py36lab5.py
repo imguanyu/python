e5fd8e7498cc992199f135e976387eed0ac73855
yu <yeh0824@mail.tcte.edu.tw>
2018-11-14 10:04:15 +0800

    2018.11.14 init start

---
+++ Py36Lab/py36lab5.py
@@ -0,0 +1,15 @@
+var1 = 'Hello WOrld'
+var2 = b"welcome"#雙引號
+var3 = '''
+Hi
+I
+am
+Mark
+'''
+var4 = '雙引號'
+var5 = '雙引號'
+print(type(var1), len(var1), var1)
+print(type(var2), len(var2), var2)
+print(type(var3), len(var3), var3)
+print(type(var4), len(var4), var4)
+print(type(var5), len(var5), var5)
